7e326bfeebb483affb6a9cd9fb122db91718225e
unknown <andreyxdd@yandex.ru>
2023-04-12 16:11:41 -0600

    Cleaned up repo

--- src/hooks/useDocumentTitle.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function useDocumentTitle(title: string): void {
-  React.useEffect(() => {
-    window.document.title = title;
-  }, [title]);
-}
-
-export default useDocumentTitle;
