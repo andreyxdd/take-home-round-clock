d40e06f0e79651cd5e1201e7712ed589e64cb784
unknown <andreyxdd@yandex.ru>
2023-04-12 18:34:08 -0600

    Added ticks and clock arrows

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -9,8 +9,55 @@ function App() {
   return (
     <div className="container">
       <div className="clock">
-        <HalfCircle side="top" bottomColor="#F8A3A8" topColor="white" />
-        <HalfCircle side="bottom" bottomColor="#F8A3A8" topColor="white" />
+        <div className="circle-halves">
+          <HalfCircle side="top" bottomColor="pink" topColor="white" />
+          <HalfCircle side="bottom" bottomColor="pink" topColor="white" />
+        </div>
+        <div className="numbers">
+          <div>1</div>
+        </div>
+        <div className="numbers">
+          <div>2</div>
+        </div>
+        <div className="numbers">
+          <div>3</div>
+        </div>
+        <div className="numbers">
+          <div>4</div>
+        </div>
+        <div className="numbers">
+          <div>5</div>
+        </div>
+        <div className="numbers">
+          <div>6</div>
+        </div>
+        <div className="numbers">
+          <div>7</div>
+        </div>
+        <div className="numbers">
+          <div>8</div>
+        </div>
+        <div className="numbers">
+          <div>9</div>
+        </div>
+        <div className="numbers">
+          <div>10</div>
+        </div>
+        <div className="numbers">
+          <div>11</div>
+        </div>
+        <div className="numbers">
+          <div>12</div>
+        </div>
+        <div className="hand" id="sec">
+          <div className="sec" />
+        </div>
+        <div className="hand" id="min">
+          <div className="min" />
+        </div>
+        <div className="hand" id="hour">
+          <div className="hour" />
+        </div>
       </div>
     </div>
   );
