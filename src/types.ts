3613b00801d773ab24463deec97a408d02963686
unknown <andreyxdd@yandex.ru>
2023-04-12 21:37:56 -0600

    Clearing up

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type Task = {
-  id: string;
-  title: string;
-  isCompleted: boolean;
-  subTasks: Array<Task>;
-}
-
-export type setTaskProps = Array<Task> | ((val: Array<Task>) => Array<Task>);
